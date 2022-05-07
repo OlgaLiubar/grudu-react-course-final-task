274dabaaaebc385832eaf0cb5c29ceaef3c0f4a9
Olga Liubar <olgaliubar@yandex.ru>
2022-05-07 22:34:44 +0200

    feat: add basic structure and markup

---
+++ twitter-clone/src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Header/Header";
+import Input from "../Input/Input";
+import Tweet from "../Tweet/Tweet";
+// import Footer from "../Footer/Footer";
+
+export default function Main() {
+  return (
+    <>
+      <Header/>
+      <Input/>
+      <Tweet/>
+      <Tweet/>
+    </>
+  );
+}
